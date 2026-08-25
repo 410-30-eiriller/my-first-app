1c2130d93680625865a00b7356011e0be7ad0b1b
410-30-eiriller <54651@yupparaj.ac.th>
2026-08-25 13:34:51 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -52,7 +52,7 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
         # ตรวจข้อ 3
         if u_ans3 == "banana":
-        st.success("✅ ข้อ 3: ถูกต้อง")
+            st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
