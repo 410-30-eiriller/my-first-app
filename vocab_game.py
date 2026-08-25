ef5e96f3a989da2112eef6314fa97b4c38a03f34
410-30-eiriller <54651@yupparaj.ac.th>
2026-08-25 13:35:25 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -55,7 +55,7 @@ def show_result_dialog(ans1, ans2):
             st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
-        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
+    st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
         # ตรวจข้อ 4
         if u_ans4 == "watermalon":
         st.success("✅ ข้อ 4: ถูกต้อง")
