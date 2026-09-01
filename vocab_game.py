c8502947f9dce3df47a0d0f9c95b6a632f4d90f4
410-30-eiriller <54651@yupparaj.ac.th>
2026-09-01 13:28:01 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -61,7 +61,7 @@ def show_result_dialog(ans1, ans2):
             st.success("✅ ข้อ 4: ถูกต้อง")
             score += 1
         else:
-        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
+            st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
     
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
     
